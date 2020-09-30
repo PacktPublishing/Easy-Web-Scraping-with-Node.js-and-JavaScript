c4bc1058aa1c4d2383cdfcaf49e69365c4078a19
Igor Savinkin <igor.savinkin@gmail.com>
2020-09-30 10:30:34 +0300

    ain project commit

---
+++ ch1/src/scrape-proxied.js
@@ -0,0 +1,81 @@
+const peto = require("peto");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const origin = "https://www.amazon.com";
+let page = 1;
+
+const scrape = (url, maxPages = 10) => {
+  let proxies = [];
+  try {
+    // read the proxies.txt file into a string
+    proxies = fs.readFileSync("proxies.txt", "utf8");
+    // make an array out of the proxies string
+    proxies = proxies.split("\n");
+    // filter out empty proxies
+    proxies = proxies.filter((proxy) => proxy.length > 0);
+  } catch (err) {
+    console.error("Could not read proxies", err);
+  }
+
+  const randomIndex = Math.floor(Math.random() * proxies.length);
+  const proxy = proxies[randomIndex];
+  console.log("Using proxy", proxy);
+
+  console.log("Opening", url);
+  // step 1
+  return peto({ url, proxy })
+    .then(({ statusCode, data, ...response }) => {
+      // exit this function if url is not well loaded
+      if (statusCode !== 200)
+        return console.error("Could not open", url, response);
+
+      console.log("Opened");
+
+      // create an array that we push scraped products into
+      let products = [];
+
+      // step 2
+      const $ = cheerio.load(data);
+
+      // steps 3 and 4
+      try {
+        $("[data-asin]").each((i, elm) => {
+          elm = $(elm);
+          const title = elm.find("h2").text().trim();
+          const price = elm.find(".a-price .a-offscreen").text().trim();
+          const rating = elm.find(".a-icon-alt").text().trim();
+          const reviews_count = elm
+            .find('a[href$="#customerReviews"]')
+            .text()
+            .trim();
+          if (title && rating)
+            products.push({ title, price, rating, reviews_count });
+        });
+
+        // step 5
+        const file = `gotten-data/products_${page}.json`;
+        fs.writeFile(file, JSON.stringify(products), (err) => {
+          if (err) return console.error("Unable to write to file", file, err);
+          console.log("Products saved to file", file);
+        });
+        console.log("products", products);
+      } catch (e) {
+        console.error("Unable to get the categories at", url, e);
+      }
+
+      // step 6 (the recursive path)
+      page++; // auto increment the value of page
+      if (page > maxPages) return;
+
+      const nextPageURL =
+        $(".a-pagination .a-last a").attr("href") ||
+        $("#pagnNextLink").attr("href");
+      if (nextPageURL) return scrape(origin + nextPageURL, maxPages);
+
+      console.log("No more pages to scrape");
+    })
+    .catch(console.error);
+};
+
+module.exports = scrape;
